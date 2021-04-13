c8c07fd6dc39d17dc98cae211242868457f40b80
Jakson Ewell <76876544+Jrewel@users.noreply.github.com>
2021-04-13 07:10:21 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 
-document.querySelector("input").click();
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.toggle("huge");
+document.querySelector("h1").classList.toggle("huge");
+document.querySelector("h1").classList.remove("huge");
